70bb6217e4df986e19f874fde29856d81a209c9c
matt-fonto <mateus.fonto@gmail.com>
2024-08-12 13:30:19 +0200

    feat: add supabase utils

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import { createClient } from "./supabase/server";
+import { createClient } from "@supabase/supabase-js";
 
-export default function Home() {
-  const supabase = createClient();
+export default async function Home() {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
 
-  console.log("supabase", supabase);
+  const supabase = createClient(supabaseUrl, supabaseKey);
+
+  let { data: todos, error } = await supabase.from("todos").select();
+
+  console.log("error", error);
+  console.log("todos", todos);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
